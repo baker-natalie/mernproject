0a117834e96b10e5f9afb6389dc053b6a5991188
Natalie Baker <natalie.baker@i.aal-usa.com>
2017-03-10 09:32:44 -0600

    add react and fullpage-react

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', { title: 'My Sample Project' });
 });
 
 module.exports = router;
